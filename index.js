cfcc363c2c57b0da208b0238a0645cebc6a5e7bd
Tuyen Nguyen <tuyennguyen@Tuyens-MacBook-Air.local>
2021-09-27 19:51:27 +1000

    Safari Flex and Smooth Scroll Fixed

--- index.js
+++ index.js
@@ -5,22 +5,37 @@ yearEl.textContent = new Date().getFullYear();
 // Put the event to the nav button
 const navButton = document.querySelector('.nav-buttons')
 const navOpen = document.querySelector('#header')
-navButton.addEventListener('click', function(event) {
+navButton.addEventListener('click', function (event) {
   navOpen.classList.toggle('nav-open')
 })
 
 //Implement Smooth Scrolling
 //Can also use scrollTo to set the specific position
-const allLinks = document.querySelectorAll('a:link')
-allLinks.forEach(function(link) {
-  console.log(link.hash)
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    const sectionEl = document.querySelector(link.hash)
-    console.log(sectionEl)
-    sectionEl.scrollIntoView({behavior: 'smooth'})
-  })
-})
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
 //Implement Sticky Navigation Bar
 const sectionHeroEl = document.querySelector('.section-hero')
@@ -28,7 +43,7 @@ const sectionHeroEl = document.querySelector('.section-hero')
 // reach the bottom edge hero section then the .sticky class 
 // will appear
 // when the hero section move out the view port, we want the nav is sticky
-const obs = new IntersectionObserver(function(entries){
+const obs = new IntersectionObserver(function (entries) {
   console.log(entries)
   if (!entries[0].isIntersecting) {
     document.body.classList.add('sticky')
@@ -44,3 +59,22 @@ const obs = new IntersectionObserver(function(entries){
   rootMargin: '-80px'
 })
 obs.observe(sectionHeroEl)
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
